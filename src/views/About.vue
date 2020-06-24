778b813e4cb56a4768f74d7eaf41900a63c035ed
hao <xuhaonju@gmail.com>
2020-06-24 15:42:39 +0200

    add footer to about vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,6 +10,27 @@
     </section>
     <img class="background-img" :src="siteConfig.background_image" />
   </div>
+  <br />
+
+  <footer class="footer">
+    <div class="columns is-moible is-centered" v-if="siteConfig.footer">
+      <div
+        v-for="item in siteConfig.footer"
+        :key="item.label"
+        class="column is-one-quarter"
+        style="text-align: center"
+      >
+        <b-tooltip :label="item.tooltip" position="is-top"
+          ><a :href="item.url" target="_blank">
+            <img :src="item.logo" style="height: 55px;" />
+          </a>
+        </b-tooltip>
+      </div>
+    </div>
+    <div style="text-align: center" v-if="siteConfig.footnote">
+      <p>{{ siteConfig.footnote }}</p>
+    </div>
+  </footer>
 </template>
 <script>
 import siteConfig from "../../site.config.json";
