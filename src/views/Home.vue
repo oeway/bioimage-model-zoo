d23cf8ad3447f77204e448a024752df5e5a1c309
oeway <oeway007@gmail.com>
2022-01-27 11:33:05 +0100

    Change notification

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,11 +11,8 @@
         aria-close-label="Close notification"
         role="alert"
       >
-        We are currently upgrading the website, to access all the previous
-        models, go to
-        <a href="https://deploy-preview-199--bioimage.netlify.app/#/"
-          >https://deploy-preview-199--bioimage.netlify.app/</a
-        >.
+        We are currently upgrading the website, more models will be available
+        soon!
       </b-notification>
       <div class="hero-body" style="position: relative;">
         <img
