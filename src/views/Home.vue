94982c0120e78c3d7fae6bd67182d1ac637d81e1
oeway <oeway007@gmail.com>
2022-03-02 00:13:59 +0100

    Restore links

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -532,13 +532,10 @@ function normalizeItem(self, item) {
   if (item.links) {
     for (let link_key of item.links) {
       const linked = self.resourceItems.filter(
-        (item) =>
-          (link_key.match(/\//g) || []).length === 2  // e.g 10.5281/zenodo.<resource nr>/<version nr> or <partner_id>/<resource id>/latest
-            ? item.id.toLowerCase() === link_key.toLowerCase()  // exact version id
-            : item.id.toLowerCase().includes(link_key.toLowerCase())  // resource id matches version id; select latest (first) below
+        item => item.id.toLowerCase() === link_key.toLowerCase()
       );
 
-      for (let lit of linked.slice(0, 1)) {
+      for (let lit of linked) {
         item.apps.unshift({
           name: lit.name,
           icon: lit.icon || DEFAULT_ICONS[lit.type],
