6a5df206bc9c0401b8c3c5a07d00bb601184c218
Wei Ouyang <oeway007@gmail.com>
2023-03-17 01:51:14 +0100

    exclude partner: bioimageio

--- src/store.js
+++ src/store.js
@@ -102,7 +102,9 @@ export const store = new Vuex.Store({
           duplicates.forEach(p => {
             siteConfig.partners.splice(siteConfig.partners.indexOf(p), 1);
           });
-          siteConfig.partners.push(c);
+          if(!siteConfig.excluded_partners || !siteConfig.excluded_partners.includes(c)){
+            siteConfig.partners.push(c);
+          }
         }
       }
       if (repo_manifest.collection && siteConfig.partners) {
