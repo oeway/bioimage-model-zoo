e0d04b8991460aa3fa46a5f2f0a31b186c7dbc15
oeway <oeway@users.noreply.github.com>
2024-02-29 16:33:59 +0000

    Deploy to GitHub Pages ea56d9cfeb145d21c32b80a5bae7200cba6eb958

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e6709e076cde92d54cfc1a84f13c4242.js"
+  "/precache-manifest.f7a15f1c46db2d9ad9b093b1d40484da.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "bioimage"});
